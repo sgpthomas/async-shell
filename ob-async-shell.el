ae944c6c859606114634a8cf0d38322808dff0af
Samuel Thomas <sgpthomas@gmail.com>
2022-04-08 11:35:23 -0500

    add proper highlighting to blocks

--- ob-async-shell.el
+++ ob-async-shell.el
@@ -4,3 +4,7 @@
   (message "body: %s" body)
   (message "params: %s" params)
   (async-shell-command body))
+
+;;;###autoload
+(define-derived-mode async-shell-mode shell-script-mode "Async Shell Mode"
+  "Major Mode for highlighting text in async-shell org babel blocks.")
