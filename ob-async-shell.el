b68e3ef112310123d895f862188020b153b6fcec
Sammy Thomas <sgpthomas@gmail.com>
2022-09-20 09:39:34 -0500

    use start-file-process instead of async command to get more control of the buffer

--- ob-async-shell.el
+++ ob-async-shell.el
@@ -1,7 +1,15 @@
 (provide 'ob-async-shell)
 
 (defun org-babel-execute:async-shell (body params)
-  (async-shell-command body))
+  (let* ((processed-params (org-babel-process-params params))
+	 (default-directory (cdr (assq :dir processed-params))))
+    (with-current-buffer "*ob-async-shell*"
+      (erase-buffer)
+      (insert "=== Command ===\n")
+      (insert body)
+      (insert "\n=== Output ===\n")
+      (shell-mode)
+      (start-file-process "ob-async-shell" "*ob-async-shell*" "/bin/bash" "-c" body))))
 
 ;;;###autoload
 (define-derived-mode async-shell-mode shell-script-mode "Async Shell Mode"
