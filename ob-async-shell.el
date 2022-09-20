bb47a1adc1fa102b5e4407c08bb6420e60acd6bf
Sammy Thomas <sgpthomas@gmail.com>
2022-09-20 16:47:09 -0500

    added a buffer-revert function

--- ob-async-shell.el
+++ ob-async-shell.el
@@ -4,12 +4,37 @@
   (let* ((processed-params (org-babel-process-params params))
 	 (default-directory (cdr (assq :dir processed-params))))
     (with-current-buffer (get-buffer-create "*ob-async-shell*")
-      (erase-buffer)
-      (insert "=== Command ===\n")
-      (insert body)
-      (insert "\n=== Output ===\n")
-      (shell-mode)
-      (start-file-process "ob-async-shell" "*ob-async-shell*" "/bin/bash" "-c" body))))
+      ;; save the command as a buffer local variable
+      (setq-local ob-async-shell-command body)
+
+      ;; set the revert buffer function
+      (setq-local revert-buffer-function
+		  'ob-async-shell-rerun)
+
+      (shell-mode))
+
+    (ob-async-shell-run body)))
+
+(defun ob-async-shell-run (command)
+  (with-current-buffer "*ob-async-shell*"
+    (setq-local buffer-read-only 'nil)
+    ;; clear buffer and then insert command body
+    (erase-buffer)
+    ;; (shell-mode)
+    (insert "=== Command ===\n")
+    (insert command )
+
+    ;; run the command using `start-file-process'
+    ;; so that it respects `default-directory'.
+    (insert "\n=== Output ===\n")
+    (start-file-process "ob-async-shell" "*ob-async-shell*" "/bin/bash" "-c" command)
+
+    (setq-local buffer-read-only t)))
+
+(defun ob-async-shell-rerun (&optional ignore-auto no-confirm)
+  (with-current-buffer "*ob-async-shell*"
+    (when ob-async-shell-command
+      (ob-async-shell-run ob-async-shell-command))))
 
 ;;;###autoload
 (define-derived-mode async-shell-mode shell-script-mode "Async Shell Mode"
