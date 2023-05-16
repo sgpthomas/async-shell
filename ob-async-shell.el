3c6b8fe3b7b5b8905ffc392effbe9bda3a28d330
Sammy Thomas <sgpthomas@gmail.com>
2023-05-16 11:48:28 -0600

    implement buffer renaming

--- ob-async-shell.el
+++ ob-async-shell.el
@@ -6,8 +6,14 @@
 
 (provide 'ob-async-shell)
 
-(defvar ob-async-shell-use-ansi t
+(defvar-local ob-async-shell-use-ansi t
   "Should ob async use ansi escape codes?")
+
+(defvar-local ob-async-shell-command nil
+  "The shell command that this buffer is bound to.")
+
+(defvar-local ob-async-shell-name nil
+  "The name of this shell buffer.")
 
 (defvar ob-async-save-hooks '()
   "List of save hooks registered.")
@@ -19,25 +25,28 @@
          (vars (if no-ansi-color
                    vars
                  (concat "export TERM=\"xterm-256color\"\n" vars)))
-         (body (s-concat "#### begin body ####\n"
+         (default-dir (if default-dir default-dir default-directory))
+         ;; append cd to the beginning because `compilation-start' expects this
+         (body (s-concat (format "cd %s\n" default-dir)
+                         "#### begin body ####\n"
                          vars "\n"
                          command "\n"
                          "#### end body ####\n"))
+         (comp-buf (compilation-start body 'async-shell-process-mode
+                                      (lambda (_)
+                                        (if ob-async-shell-name
+                                            ob-async-shell-name
+                                          buffer-name)))))
 
-         )
-    (with-current-buffer (get-buffer-create buffer-name)
+    ;; we have to set the variables after starting the compilation buffer
+    ;; otherwise these bindings are removed.
+    ;; TODO: rewrite to not use compilation buffer
+    (with-current-buffer comp-buf
       ;; save the command as a buffer local variable
-      (setq-local ob-async-shell-command body)
-      (setq-local ob-async-shell-name name)
-      (setq-local default-directory default-dir)
-      (setq-local ob-async-shell-use-ansi (not no-ansi-color)))
-
-    (defun ob-async-shell-run (command)
-      (compilation-start command
-                         'async-shell-process-mode
-                         (lambda (_) buffer-name)))
-
-    (ob-async-shell-run body)))
+      (setq-local default-directory default-dir
+                  ob-async-shell-command body
+                  ob-async-shell-name buffer-name
+                  ob-async-shell-use-ansi (not no-ansi-color)))))
 
 (defun org-babel-execute:async-shell (body params)
   (let* ((processed-params (org-babel-process-params params))
@@ -67,25 +76,55 @@
   (when ob-async-save-hooks
     (--each ob-async-save-hooks (funcall (cdr it)))))
 
+(defun ob-async-read (&optional filter-fn)
+  "Uses the `completing-read' interface to prompt the user to select and ob-async buffer"
+
+  (read-buffer "Buffer: " nil t
+               (lambda (b)
+                 (and (equal (buffer-local-value 'major-mode (cdr b))
+                             #'async-shell-process-mode)
+                      (if filter-fn
+                          (funcall filter-fn (car b))
+                        t)
+                      ))))
+
+(defun ob-async-this-buffer? ()
+  "Returns `t' if the current buffer is an async-shell buffer"
+  (equal major-mode #'async-shell-process-mode))
+
+(defun ob-async-here-or-prompt (&optional filter)
+  "Returns an ob-async buffer either by prompting or returning the current buffer"
+
+  (if (ob-async-this-buffer?)
+      (buffer-name)
+    (ob-async-read filter)))
+
 (defun ob-async-register ()
   (interactive)
 
-  (let ((buffer (read-buffer "Buffer: " nil t
-                             (lambda (b)
-                               (and (s-contains? "*ob-async-shell:" (car b))
-                                    (equal (buffer-local-value 'major-mode (cdr b))
-                                           #'async-shell-process-mode)
-                                    (not (assoc (car b) ob-async-save-hooks)))))))
+  (let ((buffer (ob-async-here-or-prompt
+                 (lambda (b) (not (assoc b ob-async-save-hooks))))))
     (add-to-list 'ob-async-save-hooks `(,buffer . ,(ob-async-update-buffer-fn buffer)))))
 
 (defun ob-async-unregister ()
   (interactive)
 
-  (let ((options (--map (car it) ob-async-save-hooks)))
-    (when options
-      (setq ob-async-save-hooks
-            (assoc-delete-all (completing-read "Buffers: " options nil t)
-                              ob-async-save-hooks)))))
+  (when ob-async-save-hooks
+    (setq ob-async-save-hooks
+          (assoc-delete-all (ob-async-here-or-prompt
+                             (lambda (b)
+                               (assoc b ob-async-save-hooks)))
+                            ob-async-save-hooks))))
+
+(defun ob-async-rename ()
+  (interactive)
+
+  (let* ((buf (ob-async-here-or-prompt)))
+    (with-current-buffer (get-buffer buf)
+      (let ((new-name
+             (format "*ob-async-shell:%s*" (read-string "New Name: "))))
+        (rename-buffer new-name)
+        (setq-local ob-async-shell-name new-name)))))
 
 ;;;###autoload
 (define-derived-mode async-shell-mode shell-script-mode "Async Shell Mode"
